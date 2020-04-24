d2c946d295ee0d8630b090c04f1127e19b9eff4d
xseivier <xavieralejandrors@gmail.com>
2020-04-24 00:18:52 -0400

    first-seccion done

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import { Button } from '@material-ui/core';
 import './index.css'
-// import {Sombras13} from '../../assets/sombras-13.png'
-// import {Sombras14} from '../../assets/sombras-14.png'
+import Sombras13 from '../../assets/sombras-13.png'
+import Sombras14 from '../../assets/sombras-14.png'
+import HomeImage from '../../assets/home-image.png'
 export default function Home() {
     return (
+      <Fragment>
+        <img src={Sombras13} alt='sombra' className='imageHomeTop'></img>
+        <img src={Sombras14} alt='sombra' className='imageHomeBottom'></img>
         <div className='home-1'>
-            <h2>
-            Estamos listos para impulsar tu negocio!
+          <div className='info-1'>
+            <h2 className='title-home-1'>
+            ¡Estamos listos para impulsar tu negocio!
             </h2>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.
             </p>
-            <Button variant="contained" color="primary">Nuestros servicios</Button>
+            <Button variant="contained" style={{background: "#4885ed", color: '#fff'}}>Nuestros servicios</Button>
+          </div>
+          <img src={HomeImage} width='700px' alt='homeImage'/>
         </div>
+      </Fragment>
+        
     )
 }
